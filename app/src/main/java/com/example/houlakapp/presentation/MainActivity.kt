6da15800ed968bba11101b7a85a4930c6e405f09
martin.delbel <martin.delbel@geopagos.com>
2023-09-01 15:10:43 -0300

    implement fragment navigation and create fragment_search_artist

---
+++ app/src/main/java/com/example/houlakapp/presentation/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.houlakapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.houlakapp.R
+import com.example.houlakapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+
+
+
+
+
+
+}
