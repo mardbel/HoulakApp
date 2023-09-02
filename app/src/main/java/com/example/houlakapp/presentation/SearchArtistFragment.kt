bd689cfeaa214f6a3633be1642d30b6a1bc727aa
martin.delbel <martin.delbel@geopagos.com>
2023-09-02 17:59:53 -0300

    fix Hilt configuration

--- app/src/main/java/com/example/houlakapp/presentation/SearchArtistFragment.kt
+++ app/src/main/java/com/example/houlakapp/presentation/SearchArtistFragment.kt
@@ -5,16 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.houlakapp.R
+import com.example.houlakapp.databinding.FragmentSearchArtistBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchArtistFragment : Fragment() {
+
+    lateinit var binding : FragmentSearchArtistBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_search_artist, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentSearchArtistBinding.bind(view)
+
+        binding.searchButton.setOnClickListener {
+            Toast.makeText(activity, "apretaste el boton", Toast.LENGTH_LONG).show()
+        }
+
+
     }
 }
